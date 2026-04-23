b97a3485ec6a839b678259aa40b513681b7fe509
rastikasinta9-lgtm <rastikasinta9@gmail.com>
2026-04-23 09:25:04 +0900

    luas segitig

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-alas = float(input("Masukkan alas: "))
+# Menghitung luas segitiga
+
+# Input alas dan tinggi
+alas = float(input("Masukkan panjang alas: "))
 tinggi = float(input("Masukkan tinggi: "))
 
+# Rumus luas segitiga
 luas = 0.5 * alas * tinggi
 
+# Output hasil
 print("Luas segitiga adalah:", luas)
